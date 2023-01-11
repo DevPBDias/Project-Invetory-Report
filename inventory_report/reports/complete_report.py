fd8a7479b073dd40e3cb1bd246ebe05533f2859c
DevPBDias <devpbdias@gmail.com>
2023-01-11 18:50:13 -0300

    fix: reducing lines and logic of SimpleReporter

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,24 @@
+
+# [
+#   {
+#     "id": 1,
+#     "nome_do_produto": "MESA",
+#     "nome_da_empresa": "Forces of Nature",
+#     "data_de_fabricacao": "2022-05-04",
+#     "data_de_validade": "2023-02-09",
+#     "numero_de_serie": "FR48",
+#     "instrucoes_de_armazenamento": "Conservar ao abrigo de luz"
+#   }
+# ]
+# Data de fabricação mais antiga: YYYY-MM-DD
+# Data de validade mais próxima: YYYY-MM-DD
+# Empresa com mais produtos: NOME DA EMPRESA
+# Produtos estocados por empresa:
+# - Physicians Total Care, Inc.: QUANTIDADE
+# - Newton Laboratories, Inc.: QUANTIDADE
+# - Forces of Nature: QUANTIDADE
+
+
 class CompleteReport:
-    pass
+    def generate():
+        pass
--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,31 +1,18 @@
 from datetime import date as dt
 from collections import Counter
 
-#    [
-#      {
-#        "id": 1,
-#        "nome_do_produto": "CADEIRA",
-#        "nome_da_empresa": "Forces of Nature",
-#        "data_de_fabricacao": "2022-04-04",
-#        "data_de_validade": "2023-02-09",
-#        "numero_de_serie": "FR48",
-#        "instrucoes_de_armazenamento": "Conservar em local fresco"
-#      }
-#    ]
-
 
 class SimpleReport:
-    @staticmethod
-    def generate(file):
+    @classmethod
+    def generate(cls, file):
         today = dt.today().strftime("%Y-%m-%d")
 
         manufacturing_dates = [item["data_de_fabricacao"] for item in file]
         oldest_manuf_date = sorted(manufacturing_dates)[0]
 
         validate_dates = [item["data_de_validade"] for item in file]
-        closest_valid_date = [
-            sorted(item for item in validate_dates if item > today)[0]
-            ]
+        closest_valid_date = sorted(
+            item for item in validate_dates if item > today)[0]
 
         companies = [item["nome_da_empresa"] for item in file]
         company_with_most_products = Counter(companies).most_common()
@@ -33,6 +20,6 @@ class SimpleReport:
 
         return (
             f"Data de fabricação mais antiga: {oldest_manuf_date}\n"
-            f"Data de validade mais próxima: {closest_valid_date[0]}\n"
+            f"Data de validade mais próxima: {closest_valid_date}\n"
             f"Empresa com mais produtos: {company}"
         )
