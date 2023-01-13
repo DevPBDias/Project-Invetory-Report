15218f1df29c1e1a5a0967a616d5584d2587165a
DevPBDias <devpbdias@gmail.com>
2023-01-13 16:38:31 -0300

    fix: change line of abstractmethod and change dictreader to load in JsonIMporter class

--- inventory_report/importer/json_importer.py
+++ inventory_report/importer/json_importer.py
@@ -7,7 +7,7 @@ class JsonImporter(Importer):
     def import_data(cls, file):
         if file.endswith(".json"):
             with open(file) as json_file:
-                data = json.DictReader(json_file)
+                data = json.load(json_file)
                 return list(data)
         else:
             return "Arquivo no formato incorreto"
--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,7 +1,7 @@
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.importer.csv_importer import CsvImporter
-from inventory_report.importer.csv_importer import JsonImporter
+from inventory_report.importer.json_importer import JsonImporter
 
 
 class Inventory:
