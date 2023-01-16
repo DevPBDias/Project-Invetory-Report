17419d27c1374fdb36ef5bca67b07e0effee53c5
DevPBDias <devpbdias@gmail.com>
2023-01-15 21:13:06 -0300

    feat: implemented xmlimporter and added xml condition to inventory class

---
+++ inventory_report/importer/xml_importer.py
@@ -0,0 +1,18 @@
+from inventory_report.importer.importer import Importer
+import xml.etree.ElementTree as ET
+
+
+# https://www.guru99.com/manipulating-xml-with-python.html
+# https://www.geeksforgeeks.org/reading-and-writing-xml-files-in-python/
+class XmlImporter(Importer):
+    @classmethod
+    def import_data(cls, file):
+        if file.endswith(".xml"):
+            tree = ET.parse(file)
+            root = tree.getroot()
+            data = []
+            for elem in root:
+                data.append({response.tag: response.text for response in elem})
+            return data
+        else:
+            return "Arquivo no formato incorreto"
--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -2,6 +2,7 @@ from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.importer.csv_importer import CsvImporter
 from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
 
 
 class Inventory:
@@ -13,6 +14,8 @@ class Inventory:
             report = CsvImporter.import_data(path)
         elif path.endswith(".json"):
             report = JsonImporter.import_data(path)
+        elif path.endswith(".xml"):
+            report = XmlImporter.import_data(path)
         return report
 
     @classmethod
